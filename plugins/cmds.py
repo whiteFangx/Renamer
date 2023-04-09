a8b6b5e577cd6efea37b403c9eef96e82368caea
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2023-04-09 22:14:23 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -23,58 +23,59 @@ from scripts import Scripted
 from pyrogram import Client as Clinton
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
-
 @Clinton.on_message(filters.command(["start"]))
 async def start(bot, update):
-          await bot.send_message(
-          chat_id=update.chat.id,
-          text=Scripted.START_TEXT,
-          parse_mode="html",
-          disable_web_page_preview=True,
-          reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text='⭕ Cʜᴀɴɴᴇʟ ⭕', url=f'https://t.me/{Config.UPDATE_CHANNEL}'),
+    message = await bot.send_message(
+        chat_id=update.chat.id,
+        text=Scripted.START_TEXT,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text='⭕ Cʜᴀɴɴᴇʟ ⭕', url=f'https://t.me/{Config.UPDATE_CHANNEL}'),
                                                  InlineKeyboardButton(text='⭕ Sᴜᴘᴘᴏʀᴛ ⭕', url=f'https://t.me/{Config.UPDATE_GROUP}') ],
                                                [ InlineKeyboardButton(text='👮 DᴇvᴇlopᴇR', url='https://t.me/TheTeleRoid'),
                                                  InlineKeyboardButton(text='🚸 Pᴏweʀᴇd By', url='https://t.me/MoviesFlixers_DL') ],
-                                               [ InlineKeyboardButton(text='🔐 Cʟᴏꜱᴇ 🔐', callback_data='DM') ] ] ) )
-
+                                               [ InlineKeyboardButton(text='🔐 Cʟᴏꜱᴇ 🔐', callback_data='DM') ] ] )
+    )
+    await update.message.delete()
 
 
 @Clinton.on_message(filters.command(["help"]))
 async def helpme(bot, update):
-          await bot.send_message(
-          chat_id=update.chat.id,
-          text=Scripted.HELP_TEXT,
-          parse_mode="html",
-          disable_web_page_preview=True,
-          reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text='🔐 ᴄʟᴏꜱᴇ', callback_data='DM') ] ] ) )
-
+    message = await bot.send_message(
+        chat_id=update.chat.id,
+        text=Scripted.HELP_TEXT,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text='🔐 ᴄʟᴏꜱᴇ', callback_data='DM') ] ] ) 
+    )
+    await update.message.delete()
 
 
 @Clinton.on_message(filters.command(["about"]))
 async def abot(bot, update):
-          await bot.send_message(
-          chat_id=update.chat.id,
-          text=Scripted.ABOUT_TEXT,
-          parse_mode="html",
-          disable_web_page_preview=True,
-          reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text='ᴄʟᴏꜱᴇ 🔐', callback_data='DM') ] ] ) )
-
+    message = await bot.send_message(
+        chat_id=update.chat.id,
+        text=Scripted.ABOUT_TEXT,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text='ᴄʟᴏꜱᴇ 🔐', callback_data='DM') ] ] )
+    )
+    await update.message.delete()
 
 
 @Clinton.on_message(filters.command(["upgrade"]))
 async def upgra(bot, update):
-          await bot.send_message(
-          chat_id=update.chat.id,
-          text=Scripted.UPGRADE_TEXT,
-          parse_mode="html",
-          disable_web_page_preview=True,
-          reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text='🔐 ᴄʟᴏꜱᴇ', callback_data='DM') ] ] ) )
-
+    message = await bot.send_message(
+        chat_id=update.chat.id,
+        text=Scripted.UPGRADE_TEXT,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text='🔐 ᴄʟᴏꜱᴇ', callback_data='DM') ] ] )
+    )
+    await update.message.delete()
 
 
 @Clinton.on_callback_query()
 async def button(bot, update):
- 
-      if  'DM'  in update.data:
-                await update.message.delete()
+    if 'DM' in update.data:
+        await update.message.delete()
